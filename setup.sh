a2db7abc37dd052c958f9c8e12a71c639e33f9ce
Vitório Edson dos Santos Silva <vitoriodev@proton.me>
2024-10-16 12:44:40 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,3 +35,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 48
 gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 18'
