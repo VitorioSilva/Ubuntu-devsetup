9c187889ea421829f18b347c3d6d7ab4ca5f0edc
Vitório Edson dos Santos Silva <vitoriosantosprofi@proton.me>
2025-04-10 13:49:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,10 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # pacotes
-sudo apt install git curl build-essential dkms perl wget -y
-sudo apt install gcc make default-libmysqlclient-dev libssl-dev -y
-sudo apt install -y zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
-  libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
+sudo apt install -y git curl build-essential dkms perl wget gcc make \
+default-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+libffi-dev liblzma-dev python3-openssl
 
 # Terminal ZSH
 sudo apt install zsh -y
@@ -30,15 +29,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Dock (barra de aplicativos)
 # parte inferior da tela
-gsettings set org.gnome.shell.extensions.dash-to-dock
-extend-height false
-gsettings set org.gnome.shell.extensions.dash-to-dock
-dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock
-transparency-mode FIXED
-gsettings set org.gnome.shell.extensions.dash-to-dock
-dash-max-icon-size 48
-gsettings set org.gnome.shell.extensions.dash-to-dock
-background-opacity 0
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 48
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
 gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 18'
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
