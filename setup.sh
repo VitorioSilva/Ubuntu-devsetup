ce9628a83950775ec81628b71c3794ecc9bd0320
Vitório Edson dos Santos Silva <vitoriosantosprofi@proton.me>
2025-05-23 16:42:00 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,5 +42,5 @@ gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.25
 gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces false
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DOTS'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 18'
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 18'
