57a6f80faf6cf04e98beac7272a27659b92ac925
Vitório Edson dos Santos Silva <vitorio.tech@proton.me>
2026-06-23 03:11:20 -0300

    Atualizar o setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,38 @@
-# atualizar os pacotes
+# Atualizar sistema
 sudo apt update
 sudo apt full-upgrade -y
+sudo apt autoremove -y
 
-# pacotes
-sudo apt install -y git curl build-essential dkms perl wget gcc make \
-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev \
-libffi-dev liblzma-dev python3-openssl python3-dev python3-venv
+# Ferramentas de desenvolvimento
+sudo apt install -y \
+git curl wget build-essential dkms perl gcc make \
+libmysqlclient-dev libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev \
+llvm libncurses-dev xz-utils tk-dev \
+libffi-dev liblzma-dev \
+python3-openssl python3-dev python3-venv
 
-# Limpar histórico do Bash
-history -c
-cat /dev/null > ~/.bash_history
-rm -f ~/.bash_history
+# Instalar ZSH
+sudo apt install -y zsh
+chsh -s "$(which zsh)"
 
-# Terminal ZSH
-sudo apt install zsh -y
-chsh -s /bin/zsh
-zsh
+# Instalar Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Instalar Oh-my-zsh! -> https://ohmyz.sh/
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Instalar plugins do Oh My Zsh
 
-# Instalar Zsh Autosuggestions
-# https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Zsh Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Instalar Zsh Syntax Highlighting
-# https://github.com/zsh-users/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Zsh Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Mudar plugins
+# Depois editar ~/.zshrc e alterar:
 # plugins=(git zsh-autosuggestions zsh-syntax-highlighting z)
 
-# Dock (barra de aplicativos)
-# parte inferior da tela
+# Configuração da Dock
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
@@ -43,4 +42,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces false
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DOTS'
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+# Fonte do terminal
 gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 18'
